f5ebaeccef396962383e21595456395f14ceb631
TheWaschtlPlays <djfubi@gmail.com>
2022-05-01 13:23:55 +0200

    added decimal numbers separated by a comma

--- Arduino/serialdummy/serialdummy.ino
+++ Arduino/serialdummy/serialdummy.ino
@@ -1,9 +1,6 @@
 //this just spits out A0 readings in the same format as "TesT" Newton-meters
 
 
-
-int randNumber;
-
 void setup() {
   Serial.begin(9600);
   pinMode(A1, OUTPUT);
@@ -11,15 +8,14 @@ void setup() {
   digitalWrite(A1, HIGH);
   digitalWrite(A2, LOW);
   
-  //randomSeed(analogRead(0));
 }
 
 void loop() {
 
-  //randNumber = (random(-3000, 3000));
-  //Serial.print(randNumber);
 
-  Serial.print(analogRead(A0) * 10);
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.print(analogRead(A0) / 20);
     
   Serial.print("  ");  //"" is the real deal
 
