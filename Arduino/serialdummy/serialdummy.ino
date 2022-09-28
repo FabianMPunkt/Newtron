65e93e9d8c768d0a6bc137bbacca8e03ecadd002
FabianMPunkt <djfubi@gmail.com>
2022-09-29 01:09:24 +0200

    Update serialdummy.ino

--- Arduino/serialdummy/serialdummy.ino
+++ Arduino/serialdummy/serialdummy.ino
@@ -1,4 +1,5 @@
 //this just spits out A0 readings in the same format as "TesT" Newton-meters
+//if pin2 is pulled low, it will stop.
 
 
 void setup() {
@@ -7,17 +8,23 @@ void setup() {
   pinMode(A2, OUTPUT);
   digitalWrite(A1, HIGH);
   digitalWrite(A2, LOW);
+
+  pinMode(2, INPUT_PULLUP);
   
 }
 
 void loop() {
 
+if(digitalRead(2) == HIGH) {
 
-  Serial.print(analogRead(A0));
-  Serial.print(",");
-  Serial.print(analogRead(A0) / 20);
-    
-  Serial.print("  ");  //"" is the real deal
+    Serial.print(analogRead(A0));
+    Serial.print(",");
+    Serial.print(analogRead(A0) / 20);
+      
+    Serial.print("  ");  //"" is the real deal
 
-  delay(30);
+    delay(30);
+
+  }
+
 }
