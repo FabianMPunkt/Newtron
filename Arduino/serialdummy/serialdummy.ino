0c6e9086f8dd9752b931438081f32dbb1b5503c1
FabianMPunkt <djfubi@gmail.com>
2022-09-29 17:42:11 +0200

    Update serialdummy.ino

--- Arduino/serialdummy/serialdummy.ino
+++ Arduino/serialdummy/serialdummy.ino
@@ -1,30 +1,54 @@
 //this just spits out A0 readings in the same format as "TesT" Newton-meters
 //if pin2 is pulled low, it will stop.
 
+bool enabled = false;
 
 void setup() {
-  Serial.begin(115200);
+
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   digitalWrite(A1, HIGH);
   digitalWrite(A2, LOW);
 
   pinMode(2, INPUT_PULLUP);
-  
 }
 
 void loop() {
 
-if(digitalRead(2) == HIGH) {
+  if (digitalRead(2) == HIGH) {
+
+    if (enabled == false) {
+
+      Serial.begin(115200);
+
+      Serial.println();
+      Serial.println("DSI Transmission enabled");
+      enabled = true;
+
+      delay(100);
+    }
 
     Serial.print(analogRead(A0));
     Serial.print(",");
     Serial.print(analogRead(A0) / 20);
-      
+
     Serial.print("  ");  //"" is the real deal
 
-    delay(30);
+    delay(10);
 
   }
 
+  else {
+
+    if (enabled == true) {
+
+      Serial.println();
+      Serial.println("DSI Transmission disabled");
+
+      Serial.end();
+      enabled = false;
+
+      delay(100);
+    }
+  }
 }
